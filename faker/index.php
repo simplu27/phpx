9469af29784fddd2dcc59229a9aa7459b544c353
simplu27 <simplu27@yahoo.com>
2016-05-19 17:04:25 +0300

    testing faker

--- faker/index.php
+++ faker/index.php
@@ -1,3 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Faker Test</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" media="screen" href="style.css">
+<style>
+	.center {
+		max-width: 800px;
+		margin: 20px auto;
+		padding: 10px;
+	}
+</style>
+</head>
+<body>
+<div class="center">
+
 <?php
 // require the Faker autoloader
 require_once 'autoload.php';
@@ -8,7 +26,7 @@ $faker = Faker\Factory::create('ro_RO');
 
 for ($i=0; $i < 3 ; $i++) { 
 	// generate data by accessing properties
-	echo "Name: " . $faker->name;
+	echo "Name: " . $faker->name($prefix=NULL);
 	  // 'Lucy Cechtelar';
 	echo "<br>";
 	echo "Address: " . $faker->address;
@@ -23,5 +41,29 @@ for ($i=0; $i < 3 ; $i++) {
 	echo "<br>";
 	echo "Company: " . $faker->company;
 
+	echo "<br>";
+	echo "CNP: " . $faker->cnp;
+
 	echo "<hr>";
 }
+
+for ($i = 0; $i < 20; $i++)
+{
+  $users[$i] = array(
+    'username' => $faker->userName,
+    'first_name' => $faker->firstName,
+    'last_name' => $faker->lastName,
+    'email' => $faker->email,
+    'password' => $faker->word,
+    'url' => $faker->url
+  );
+}
+
+echo "<pre>";
+	var_dump($users);
+echo "</pre>";
+
+?>
+</div><!-- center -->
+</body>
+</html>
