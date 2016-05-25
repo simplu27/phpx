e66ab93d533f145e44ed50a3eb5ef6e5f524cad6
simplu27 <simplu27@yahoo.com>
2016-05-25 17:48:27 +0300

    testing function

--- function/index.php
+++ function/index.php
@@ -2,6 +2,8 @@
 
 function media_aritmetica() {
 	$suma = '';
+	$indecsi = '';
+	$cifre = '';
 
 	// Functia func_num_args() intoarce numarul de argumente din functia media_aritmetica()
 	$items = func_num_args();
@@ -10,19 +12,36 @@ function media_aritmetica() {
 
 		$suma += func_get_arg($i);
 
-		$indecsi = func_get_arg($i) . ", ";
-
+		if ($i == ($items - 1)) {
+			$indecsi .= func_get_arg($i);
+			$cifre .= func_get_arg($i);
+		} else {
+			$indecsi .= func_get_arg($i) . ", ";
+			$cifre .= func_get_arg($i) . "+";
+		}
 	}
 
 	// Aflam media, impartind suma la nr. de $items
 
 	$media = $suma / $items;
 
-	echo $indecsi;
-	echo $media;
+	echo "Media aritmetica pentru <strong>" . $indecsi . "</strong> este <strong>" . $media . "</strong><br>";
+	echo "<strong>(" . $cifre . ") / ". $items ."</strong> = <strong>" . $media . "</strong><br>";
+	// echo $indecsi;
+	// echo "<br>";
+	// echo $media;
 
 } // functia media_aritmetica
 
-media_aritmetica(20, 14);
+media_aritmetica(20, 14, 6, 0, 40);
+
+echo "<br>";
+
+media_aritmetica(3, 5, 68, 1680);
+
+
+
+
+
 
 ?>
