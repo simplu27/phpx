282c02e85a104a9c181aac345099144c7a431dae
simplu27 <simplu27@yahoo.com>
2016-07-29 15:52:12 +0300

    Quiz testing
    Online quiz form in php

---
+++ quiz/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Basic Quiz Form</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" media="screen" href="style.css">
+</head>
+<body>
+
+
+<div class="center">
+	<form action="" id="quiz" class="quiz">
+		<ul>
+
+			<!-- Intrebarea 1 -->
+			<li>
+				<h3>Prima intrebare:</h3>
+				<div class="raspuns">
+					<input type="radio" name="rasp1" id="rasp1-1" value="">
+					<label for="rasp1-1">Primul raspuns</label>
+				</div><!-- raspuns -->
+				<div class="raspuns">
+					<input type="radio" name="rasp1" id="rasp1-2" value="">
+					<label for="rasp1-2">Secundul raspuns</label>
+				</div><!-- raspuns -->
+				<div class="raspuns">
+					<input type="radio" name="rasp1" id="rasp1-3" value="">
+					<label for="rasp1-3">Raspunsul 3</label>
+				</div><!-- raspuns -->
+			</li>
+
+			<!-- Intrebarea 2 -->
+			<li>
+				<h3>A doua intrebare din Quiz:</h3>
+				<div class="raspuns">
+					<input type="radio" name="rasp2	" id="rasp2	-1" value="">
+					<label for="rasp2	-1">Primul raspuns</label>
+				</div><!-- raspuns -->
+				<div class="raspuns">
+					<input type="radio" name="rasp2	" id="rasp2	-2" value="">
+					<label for="rasp2	-2">Secundul raspuns</label>
+				</div><!-- raspuns -->
+				<div class="raspuns">
+					<input type="radio" name="rasp2	" id="rasp2	-3" value="">
+					<label for="rasp2	-3">Raspunsul 3</label>
+				</div><!-- raspuns -->
+			</li>
+
+			<!-- Intrebarea 3 -->
+			<li>
+				<h3>Super intrebarea 3:</h3>
+				<div class="raspuns">
+					<input type="radio" name="rasp3	" id="rasp3	-1" value="">
+					<label for="rasp3	-1">Numero Uno</label>
+				</div><!-- raspuns -->
+				<div class="raspuns">
+					<input type="radio" name="rasp3	" id="rasp3	-2" value="">
+					<label for="rasp3	-2">Secuno</label>
+				</div><!-- raspuns -->
+				<div class="raspuns">
+					<input type="radio" name="rasp3	" id="rasp3	-3" value="">
+					<label for="rasp3	-3">3 is my favorite</label>
+				</div><!-- raspuns -->
+			</li>
+		</ul>
+	</form>
+</div><!-- center -->
+
+
+</body>
+</html>
