b81cfb8ef419e1e98d1559fac90ea38d25334fa1
simplu27 <simplu27@yahoo.com>
2016-12-19 18:13:35 +0200

    basic table styles css

--- csv/array-from-csv.php
+++ csv/array-from-csv.php
@@ -15,14 +15,47 @@ $table = extractCsv($filename, true, ",");
 <meta charset="utf-8">
 <title>Table Array from CSV</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" media="screen" href="style.css">
+<style>
+	table.table-style-1 {
+		display: block;
+		width: 100%;
+		border-collapse: collapse;
+	}
+
+	table.table-style-1 tr {
+		border: 1px solid #DFDFDF;
+	}
+
+	table.table-style-1 tr:nth-of-type(odd) {
+		background: #F7F4F4;
+	}
+
+	/*table.table-style-1 tr td:nth-of-type(1) {
+		background: #D3D3D3;
+	}*/
+
+	table.table-style-1 tr th {
+		color: #FFF;
+		background: #333;
+		text-align: left;
+	}
+
+	table.table-style-1 tr td,
+	table.table-style-1 tr th {
+		padding: 5px 20px;
+	}
+
+	table.table-style-1 tr:hover {
+		background: #7A7A7A;
+		color: #F9F9F9;
+	}
+</style>
 </head>
 <body>
-	<table>
+	<table class="table-style-1">
 		<tr>
 			<th>Id</th>
 			<th>Name</th>
-			<th>Email</th>
 			<th>Email</th>
 			<th>Date</th>
 		</tr>
